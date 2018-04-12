50170904cd76f0f45a62d35f904d96cbe3b50510
Chinmay Kulkarni <chinmay90kulkarni@gmail.com>
2018-04-12 23:26:21 +0530

    Added dictionary!
    Maintaining names of employees and their apps using dictionary!

--- APP Remainder/APPRemainder.py
+++ APP Remainder/APPRemainder.py
@@ -9,17 +9,22 @@ file = open("names","r");
 file = file.read().split("\n");    
 records = dict();   
                     
-def checkOwner(line):
-    for name in file:
-        if name in line:
-            print line;
-            break;
+def checkOwner(line):           
+    for name in file:       
+        if name in line:    
+            if name in records:
+                records[name].append(line);
+            else:               
+                records[name]=[line];
+            break;          
+
 
 for line in result.splitlines():
    checkOwner(line.lower());
         
-
-
+for record in records:
+    print(record);
+    print(records[record]); 
 
 
 
